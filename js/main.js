c888d1e9fe2bd89e5ea255203df3163282daeab2
Georg Weibel <georg.weibel@gmail.com>
2024-03-23 00:09:31 -0300

    Se agrega sección de opinión

--- js/main.js
+++ js/main.js
@@ -10,11 +10,41 @@ const missionButton = document.querySelector('#mission_button');
 const missionArrow  = document.querySelector('#mission_arrow');
 const missionInfo   = document.querySelector('#mission_info');
 
-//event:
+//variables mas/menos values:
+const masButon = document.querySelector('#boton_mas');
+const menosButon = document.querySelector('#boton_menos')
+const commentsSection = document.querySelector('.comments_section');
+const comments = document.querySelector('#comments');
+const manantialBox = document.querySelector('#manantial')
+
+
+//event mission/vission:
 visionButton_container.addEventListener('click' , visionShowInfo);
 missionButton_container.addEventListener('click' , missionShowInfo);
 
-//functions: 
+//event mas/menos:
+//masButon.addEventListener('click', mas_menos_action)
+//menosButon.addEventListener('click', mas_menos_action)
+
+masButon.addEventListener('click', function() {
+    commentsSection.classList.remove('close');
+    masButon.classList.toggle('close');
+    menosButon.classList.toggle('close');
+    comments.classList.add('open');
+    manantialBox.classList.add('open');
+
+});
+
+menosButon.addEventListener('click', function() {
+    commentsSection.classList.add('close');
+    masButon.classList.toggle('close');
+    menosButon.classList.toggle('close');
+    comments.classList.remove('open');
+    manantialBox.classList.remove('open');
+});
+
+
+//functions mission/vission: 
 
 function visionShowInfo(){
     visionArrow.classList.toggle('rotate');
@@ -37,6 +67,12 @@ function missionShowInfo(){
         missionButton.innerText ='Cerrar';
     };
 }
+
+//functions mas/menos:
+//function mas_menos_action(){
+  //  masButon.classList.toggle('close');
+    //menosButon.classList.toggle('close');
+//}
 
 
 
